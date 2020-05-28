671f614c988f9770e5204285ff228a02136fa862
Andrey Konovalov <andreyknvl@google.com>
2020-05-28 21:27:20 +0200

    TEMP: usb-fuzzer: limit number of calls to 8

--- prog/rand.go
+++ prog/rand.go
@@ -18,10 +18,10 @@ import (
 
 const (
 	// "Recommended" number of calls in programs that we try to aim at during fuzzing.
-	RecommendedCalls = 20
+	RecommendedCalls = 8
 	// "Recommended" max number of calls in programs.
 	// If we receive longer programs from hub/corpus we discard them.
-	MaxCalls = 40
+	MaxCalls = 8
 )
 
 type randGen struct {
